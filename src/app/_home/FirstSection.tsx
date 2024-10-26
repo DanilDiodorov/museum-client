88a481f06983ad7deae9c3a590548d7f9e54b924
danildiodorov <ddldiodorov@gmail.com>
2024-10-26 17:11:49 +0900

    changed name

--- src/app/_home/FirstSection.tsx
+++ src/app/_home/FirstSection.tsx
@@ -6,9 +6,7 @@ export default function FirstSection() {
             <div className="container pt-10">
                 <div className="grid-cols-2 md:grid">
                     <div>
-                        <h1 className="text-4xl font-bold">
-                            Музей истории МКОУ СОШ №7
-                        </h1>
+                        <h1 className="text-4xl font-bold">Музей истории</h1>
                         <div className="mt-5 text-xl">
                             Школьный музей-это общественное объединение,
                             созданное совместными усилиями педагогов и учащихся
