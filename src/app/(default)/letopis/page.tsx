473dffe63424a3d8306410d8803f23a76371e5ad
danildiodorov <ddldiodorov@gmail.com>
2024-11-01 17:21:28 +0900

    Описание летопися школы

--- src/app/(default)/letopis/page.tsx
+++ src/app/(default)/letopis/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next'
 import Letopis from './Letopis'
+
+export const metadata: Metadata = {
+    title: 'Летопись школы',
+    description: 'Летопись школы',
+}
 
 export default function LetopisPage() {
     return <Letopis />
