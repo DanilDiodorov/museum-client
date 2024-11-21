928ddbc3d7d0b45b4adca0fbe8b5a1a525e23eb4
danildiodorov <ddldiodorov@gmail.com>
2024-11-21 22:13:04 +0900

    fixed: title and description

--- src/app/(default)/_home/FirstSection.tsx
+++ src/app/(default)/_home/FirstSection.tsx
@@ -1,3 +1,4 @@
+import { SEO } from '@/configs/seo.config'
 import { TypeAnimation } from 'react-type-animation'
 
 export default function FirstSection() {
@@ -6,18 +7,8 @@ export default function FirstSection() {
             <div className="container pt-10">
                 <div className="grid-cols-2 md:grid">
                     <div>
-                        <h1 className="text-4xl font-bold">
-                            {/* eslint-disable-next-line react/no-unescaped-entities */}
-                            Музей истории истории школы МКОУ СОШ №7
-                        </h1>
-                        <div className="mt-5 text-xl">
-                            Школьный музей-это общественное объединение,
-                            созданное совместными усилиями педагогов и учащихся
-                            и являющееся элементом дополнительного образования и
-                            воспитания детей. Использование материалов музея в
-                            обучении активизирует учебный процесс и поднимает у
-                            учащихся интерес к знаниям вообще.
-                        </div>
+                        <h1 className="text-4xl font-bold">{SEO.title}</h1>
+                        <div className="mt-5 text-xl">{SEO.description}</div>
                         <div className="text-lg lg:text-2xl mt-14">
                             Направления:{' '}
                             <TypeAnimation
--- src/components/AppLogo.tsx
+++ src/components/AppLogo.tsx
@@ -1,16 +1,17 @@
+import { SEO } from '@/configs/seo.config'
 import Link from 'next/link'
 import { FaSchool } from 'react-icons/fa'
 
 const AppLogo = () => {
     return (
         <Link href="/">
-            <div className="flex items-center gap-3 w-[250px]">
+            <div className="flex items-center gap-3 w-[270px]">
                 <div>
                     <div className="w-[40px] h-[40px] bg-white rounded-xl grid">
                         <FaSchool className="text-primary text-[30px] place-self-center" />
                     </div>
                 </div>
-                <span>Музей истории школы МКОУ СОШ №7</span>
+                <span>{SEO.title}</span>
             </div>
         </Link>
     )
