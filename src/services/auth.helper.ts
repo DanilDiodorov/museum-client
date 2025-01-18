32a3372038ae9869ba9d5e8c2cf98bfa001499cf
danil.diodorov <diodorov.dl@rcitsakha.ru>
2025-01-18 19:50:35 +0900

    domain: 'kutanaschoolmuseum.ru'

--- src/services/auth.helper.ts
+++ src/services/auth.helper.ts
@@ -8,7 +8,7 @@ export const getAccessToken = () => {
 
 export const saveTokenStorage = (accessToken: string) => {
     Cookies.set(EnumTokens.ACCESS_TOKEN, accessToken, {
-        domain: 'localhost',
+        domain: 'kutanaschoolmuseum.ru',
         sameSite: 'strict',
         expires: 1,
     })
